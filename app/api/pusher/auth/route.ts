4c84add0b6a096ac67d6ca6ab9256270e74062a6
Sora <mukuge.sora.67@gmail.com>
2025-12-10 16:14:11 +0900

    t

---
+++ app/api/pusher/auth/route.ts
@@ -0,0 +1,15 @@
+import { getPusherInstance } from "@/libs/server";
+
+const pusherServer = getPusherInstance();
+
+export async function POST(req: Request) {
+  console.log("authenticating pusher perms...")
+  const data = await req.text();
+  const [socketId, channelName] = data
+    .split("&")
+    .map((str) => str.split("=")[1]);
+
+  const authResponse = pusherServer.authorizeChannel(socketId, channelName);
+
+  return new Response(JSON.stringify(authResponse));
+}
